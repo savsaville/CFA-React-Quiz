242ed5347ba36f48fcb36edfca881e2a57509b94
Richard Saville <richardasaville@gmail.com>
2017-05-16 14:32:54 +1000

    renders different information depending on quiz progress

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import './App.css';
 import Question from './Question';
 import ProgressBar from './ProgressBar';
 import MultiChoice from './MultiChoice';
+import Results from './Results';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       progress: 0,
-      selected: 'Pick One!'
+      selected: 'Pick One!',
+      score: 0
     };
     this.updateSelected = this.updateSelected.bind(this);
+    this.submitAnswer = this.submitAnswer.bind(this);
     this.quiz_data = [
       {
         question: 'what is the meaning of life?',
@@ -22,8 +25,28 @@ class App extends Component {
         question: 'What is the best cake?',
         correct_answer: 'Steak',
         possible_answers: [ 'Chocolate Cake', 'Cheesecake', 'Pavlova', 'Steak']
+      },
+      {
+        question: 'who is the worst at table tennis in the class?',
+        correct_answer: 'Josh',
+        possible_answers: [ 'Josh', 'Josh', 'Josh', 'Josh']
       }
     ]
+  }
+
+  submitAnswer() {
+    if (this.state.selected === this.quiz_data[this.state.progress].correct_answer) {
+      this.setState({
+        score: this.state.score + 1,
+        progress: this.state.progress + 1,
+        selected: 'Pick One!'
+      })
+    } else {
+      this.setState({
+        progress: this.state.progress + 1,
+        selected: 'Pick One!'
+      })
+    }
   }
 
   updateSelected(answer) {
@@ -34,13 +57,23 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
+      <div>
         <h2>Quiz App</h2>
-        <Question current_question={this.quiz_data[this.state.progress].question} />
-        <ProgressBar current_step={this.state.progress + 1} question_length={this.quiz_data.length} />
-        <MultiChoice
-          answers={this.quiz_data[this.state.progress].possible_answers}
-          updateSelected={this.updateSelected} />
+
+        {this.state.progress < this.quiz_data.length ? (
+          <div>
+            <Question current_question={this.quiz_data[this.state.progress].question} />
+            <ProgressBar current_step={this.state.progress + 1} question_length={this.quiz_data.length} />
+            <MultiChoice
+              answers={this.quiz_data[this.state.progress].possible_answers}
+              updateSelected={this.updateSelected}
+              handleSubmit={this.submitAnswer}
+              selectedAnswer={this.state.selected} />
+          </div>
+        )
+        : (
+        <Results score={this.state.score} end_message="Congratulations, you have finished!" />
+         )}
       </div>
     );
   }
--- src/MultiChoice.js
+++ src/MultiChoice.js
@@ -5,7 +5,9 @@ class MultiChoice extends Component {
     return (
       <div>
         {this.props.answers.map((answer, i) => <button key={i} onClick={() => this.props.updateSelected(answer)}> {answer} </button>)}
-        <button>Submit</button>
+        <br/>
+        <p> You have selected: {this.props.selectedAnswer} </p>
+        <button onClick={this.props.handleSubmit}>Submit</button>
       </div>
     )
   }
